98bb78db0cab74f6d31cb44485380aee6c4f8773
Alejandro Martinez <amartinez2@carnival.com>
2021-06-15 23:38:17 -0500

    Using args and commands to handle the responses

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-require("dotenv").config();
+require('dotenv').config();
 
 client.login(process.env.TOKEN);
 
@@ -9,17 +9,22 @@ client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
 });
 
+client.on('message', (message) => {
+	if (!message.content.startsWith("!") || message.author.bot) return;
 
-client.on('message', (message) => { 
-    console.log(message.content);
-    if (message.channel.id == '854565249898381313' && message.content === 'Jota C') {
-        message.channel.send('Quue pedo prros! 🚀');
-    }
-})
+	const args = message.content.slice("!".length).trim().split('/ +/');
+	const command = args.shift().toLowerCase();
 
-client.on('interaction', async interaction => {
-	if (!interaction.isCommand()) return;
-	if (interaction.commandName === 'ping') {
-		await interaction.reply('Pong!');
-	} 
+	if (command === "agenda") {
+		message.channel.send(`Command name: ${command}\nArguments: ${args}`);
+	} else if (command === 'scrims') {
+		message.reply("Tenemos scrim hoy Miercoles a las 7pm 🔥 🐉 vs Naguara Gaming!");
+	}
 });
+
+// client.on('interaction', async (interaction) => {
+// 	if (!interaction.isCommand()) return;
+// 	if (interaction.commandName === 'ping') { 
+// 		await interaction.reply('Pong!');
+// 	}
+// });
